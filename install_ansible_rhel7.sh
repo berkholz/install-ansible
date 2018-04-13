4027a1f5f3d0486ea86bb29dcccb2c297a02aa31
berkholz <code@berkholz.org>
2018-04-13 15:04:56 +0200

    add comments add kerberos client tool for getting krb tickets for auth

--- install_ansible_rhel7.sh
+++ install_ansible_rhel7.sh
@@ -3,6 +3,12 @@
 # enable repo via subscription-manager
 subscription-manager repos --enable rhel-7-server-extras-rpms
 
+# ansible packages
 yum install ansible-doc ansible
+
+# versioning and connection tools
 yum install git openssh-client
+
+# kerberos client tools
+yum install krb5-workstation
 
