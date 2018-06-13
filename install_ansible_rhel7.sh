234eb1f8e7845a92a6be715c5fe81ff07ac31908
berkholz <berkholz@users.noreply.github.com>
2018-06-13 17:55:48 +0200

    Tools for Windows deploying added

--- install_ansible_rhel7.sh
+++ install_ansible_rhel7.sh
@@ -12,3 +12,24 @@ yum install git openssh-client
 # kerberos client tools
 yum install krb5-workstation
 
+###
+### WINDOWS staff
+###
+### if you want to deploy windows machines remove comments 
+
+#install kerberos tools
+#yum install python-devel krb5-devel krb5-libs krb5-workstation
+
+## install pip for winrm wrappers
+# for compiling python wrappers we need gcc
+#yum install gcc.x86_64
+
+# we also need pip for obtaining the source
+#yum install python2-pip.noarch
+
+# we need the newest pip version to avoid incompatibilities
+#pip install --upgrade pip
+
+# now we can install the python wrappers ;)
+#pip install pywinrm[kerberos]
+
